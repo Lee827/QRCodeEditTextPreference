506f110c8ec9524d32047c04cefd2127b9588af6
Elmo Lee <elmolee@cathy-pc.web-on.org>
2020-08-11 13:36:27 +0800

    Add GitHub maven gradle plugin

--- qrcodeedittext/src/androidTest/java/com/elmolee/qrcodeedittext/ExampleInstrumentedTest.java
+++ qrcodeedittext/src/androidTest/java/com/elmolee/qrcodeedittext/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.elmolee.qrcodeedittextpreferencelibrary.test", appContext.getPackageName());
+        assertEquals("com.elmolee.qrcodeedittext.test", appContext.getPackageName());
     }
 }
